bac078cf2dbd81b8adb56ad3cf22b07d0c5340ef
lareyes72 <lareyes72@hotmail.com>
2021-03-22 15:42:06 -0500

    Ver req

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
+  res.end(req)
 })
 
 server.listen(port, () => {
