6ca1b92951ec81f53e051301f360d8399193cd16
lareyes72 <lareyes72@hotmail.com>
2021-03-22 16:21:23 -0500

    Fix mensaje req

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use extrict'
 
+
 const http = require('http')
-const { stringify } = require('node:querystring')
+//const { stringify } = require('node:querystring')
 
 const port = process.env.PORT
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end(stringify(req))
+  res.end(JSON.stringify(req))
 })
 
 server.listen(port, () => {
