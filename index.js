a024a769f77956666a9ad98dc52683012216b39d
lareyes72 <lareyes72@hotmail.com>
2021-03-22 16:32:15 -0500

    Req completo

--- index.js
+++ index.js
@@ -7,9 +7,19 @@ const http = require('http')
 const port = process.env.PORT
 
 const server = http.createServer((req, res) => {
+  
+  let body = [];
+  req.on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    // at this point, `body` has the entire request body stored in it as a string
+  });
+  
+  
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end(JSON.stringify(req))
+  res.end(body)
 })
 
 server.listen(port, () => {
