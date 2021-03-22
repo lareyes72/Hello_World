fb574ba5eebe332924641954e452d064a4338bb3
lareyes72 <lareyes72@hotmail.com>
2021-03-22 16:04:46 -0500

    Mensaje con el metodo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end('Hola nuevamente!!!')
+  res.end(req.method)
 })
 
 server.listen(port, () => {
